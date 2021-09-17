4cb574a82b991089087b8f3d103cdc7331316062
JohanDev6 <johanhenrique15@gmail.com>
2021-09-17 00:11:55 -0300

    Init Commit

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components'
+
+import { MainProvider, MainContext, IProviderData } from './providers/mainProvider'
+
+import Routes from './routes';
+import GlobalStyles from './styles/globalStyles'
+
+const Root = () => {
+
+  const { data } : IProviderData | any = useContext(MainContext)
+
+  return(
+      <ThemeProvider theme={data?.theme}>
+          <GlobalStyles/>
+          <Routes/>a
+      </ThemeProvider>
+  )
+}
+
+ReactDOM.render(
+  <MainProvider>
+    <Root/>
+  </MainProvider>
+  ,
+  document.getElementById('root')
+);
